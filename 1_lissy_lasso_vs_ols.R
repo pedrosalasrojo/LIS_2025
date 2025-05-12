3e8c3afa59007aef40d9ca5e4ac9709bc43c8f78
Pedro Salas <p.salas-rojo@lse.ac.uk>
2025-05-12 16:48:08 +0200

    update file 1

--- 1_lissy_lasso_vs_ols.R
+++ 1_lissy_lasso_vs_ols.R
@@ -41,31 +41,31 @@ data2 <- setp(paste0(ccyy,'p'))
 # Merge by ID 
 data <- merge(data1, data2, by=c("hid"), sort=TRUE) 
   
-# Arrange the data, just fix age, get pilabour over 10€ and arrange to show USD2017 with PPP
+# Arrange the data, just fix age, get positive pilabour and adjust to USD2017 with PPP
 
 data <- data %>% 
-    dplyr::filter(age>=30 & age<=60 & pilabour > 10) %>%
-    dplyr::mutate(pilabour = pilabour/	0.799178,
+    dplyr::filter(age>=30 & age<=60 & pilabour > 0) %>%
+    dplyr::mutate(pilabour = pilabour/0.799178,
                   age5 = cut(age, breaks = seq(min(age), max(age), 5), 
                              right = FALSE, 
                              ordered_result = TRUE),
                   age5num = as.numeric(age5)) 
   
-  # Get missing information. Drop item non responses.
-  print(summary(is.na(data))) 
-  data <- na.omit(data)
-  print(summary(is.na(data))) 
+# Get missing information. Drop item non responses.
+print(summary(is.na(data))) 
+data <- na.omit(data)
+print(summary(is.na(data))) 
   
-  # Get subsample (the algorithm runs faster with a smaller sample)
-  set.seed(3)
-  data <- data[sample(1:nrow(data), 3000, replace = FALSE),]  
+# Get subsample (the algorithm runs faster with a smaller sample)
+set.seed(3)
+data <- data[sample(1:nrow(data), 4000, replace = FALSE),]  
   
-  # See variables, check categories and levels make sense with METIS documentation
+# See variables, check categories and levels make sense with METIS documentation
   
-  for(i in c("sex", "age5num", "marital", "disabled", "status1", "educlev", "ind1_c", "occ1_c")){
+for(i in c("sex", "age5num", "marital", "disabled", "status1", "educlev", "ind1_c", "occ1_c")){
     print(i)
     print(table(data[[i]]))
-  }
+}
   
   #################################
   #
@@ -81,7 +81,7 @@ data <- data %>%
   print(summary(reg))
   coef_df <- summary(reg)$coefficients
   
-  # Run Cross Validation (alpha 1, lambda 0, OLS).
+  # Run Cross Validation (alpha 1, lambda 0, OLS). This way, we easily get a simple measure of OOS RMSE
 
   ols_tr <- caret::train(model,
                          data = data,
@@ -104,16 +104,15 @@ data <- data %>%
   #################################
 
   set.seed(3)
-  
-  # EXERCISE 1
  
   # Define model and lambda
   model <- pilabour ~ sex + factor(marital) + factor(educlev) + factor(age5num) +
                     factor(ind1_c) + factor(occ1_c) + disabled + factor(status1)
+
+  # Set el lambda a 225, as an example. Use "any" lambda you like, just to try. 
+  lambda_try <- 225       
   
-  lambda_try <- 225       # Set el lambda a 225, as an example
-  
-  # Use caret package to check RMSE
+  # Use caret package to check RMSE associated with your lambda_try
   lasso_tr <- caret::train(model,
                            data = data,
                            method = "glmnet",
@@ -124,21 +123,19 @@ data <- data %>%
   
   print(paste0("The RMSE of this model, with a lambda of ",lambda_try,", is: ", round(mean(lasso_tr[["resample"]][["RMSE"]]), 2)))
   
-  # EXERCISE 2: Train LASSO to select opt* lambda
+  # Train LASSO to select opt* lambda (the one leading to the "best" OOS RMSE, the best prediction)
   # Define LASSO setting 
-  
   dep <- data$pilabour
-  vec <- model.matrix(~ sex + factor(marital) + factor(ind1_c) + 
-                      factor(occ1_c) + disabled  + factor(status1) + 
+  vec <- model.matrix(~ sex + factor(marital) + factor(ind1_c) + factor(occ1_c) + disabled  + factor(status1) + 
                       factor(educlev) + factor(age5num), data)
 
-  # Define optimum lambda grid
+  # Define optimum lambda grid (you can also explore by trying your own values... this is faster)
   exploremodel <- glmnet::cv.glmnet(x = vec, y = dep, alpha = 1)
   range(exploremodel$lambda)
   lambda_range <- exp(seq(log(min(exploremodel$lambda)), log(max(exploremodel$lambda)), length.out = 50))
   print(lambda_range)
 
-  # Define model and lambda
+  # Define model and run tuning
   model <- pilabour ~ sex + factor(marital) + factor(educlev) + factor(age5num) + factor(status1) +
                     factor(ind1_c) + factor(occ1_c) + disabled
 
@@ -153,8 +150,6 @@ data <- data %>%
   print(plot(y = lasso_tr$results$RMSE, x = lasso_tr$results$lambda,
              main = "RMSE by lambda value", xlab = "Lambda", ylab = "RMSE"))
   print(abline(v = lasso_tr[["bestTune"]][["lambda"]]))  
-  
-  results <- lasso_tr[["results"]]
   lambda <- lasso_tr[["bestTune"]][["lambda"]]         
   rmse_lasso <- round(mean(lasso_tr[["resample"]][["RMSE"]]), 2) 
     
@@ -169,7 +164,7 @@ data <- data %>%
   print(paste0("Out of sample RMSE from LASSO is: ", rmse_lasso))
   print(paste0("LASSO improves RMSE by ", round((rmse_ols - rmse_lasso)/rmse_ols*100, 2), "%"))
 
-  # Get LASSO and get coefficients
+  # Get LASSO and get coefficients. You can use the lasso_mod object to predict and work as you need.
   
   lasso_mod <- glmnet(vec, dep, alpha=1, lambda = lambda)
   coeff_lasso <- lasso_mod$beta 
